99316c25a78d1b7b1c792ff2f130aeb26e72a12b
oneshell <oneshell@MyMBP-WiFi.lan>
2024-07-16 16:56:40 +0800

    init

---
+++ GhidraInit.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+GHIDRA_INSTALL_DIR=/Applications/ghidraRun.app/Contents/MacOS
+HEADLESS=$GHIDRA_INSTALL_DIR/support/analyzeHeadless
+PROJECT_DIR=/Users/oneshell/Documents/Ghidra_Project
+
+print_help() {
+    echo "usage: $0 -r rootfs [-b binary]"
+    echo "create a Ghidra Project from rootfs"
+    echo " -r rootfs : rootfs directory"
+    echo " -b binary : binary file"
+    echo " -h : print this help"
+}
+
+# check args
+ROOTFS=""
+BINARY=""
+while getopts "r:b:h" opt; do
+    case $opt in
+        r)
+            ROOTFS=$OPTARG
+            ;;
+        b)
+            BINARY=$OPTARG
+            ;;
+        h)
+            print_help
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: $OPTARG" 1>&2
+            print_help
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." 1>&2
+            print_help
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$ROOTFS" ]; then
+    echo "rootfs is required" 1>&2
+    print_help
+    exit 1
+fi
+
+echo "ROOTFS=$ROOTFS"
+if [ -n "$BINARY" ]; then
+    echo "BINARY=$BINARY"
+fi
+
+# 1. create project from rootfs and import all libraries
+
+
+# 2. import binary if exists
